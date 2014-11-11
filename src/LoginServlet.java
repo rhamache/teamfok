7d0ff2a88e5984a77abd78c4037d4da04a415a44
rhamache <rhamache@ualberta.ca>
2014-11-10 17:28:45 -0700

    hey

--- src/LoginServlet.java
+++ src/LoginServlet.java
@@ -1,4 +1,5 @@
 import java.io.*;
+
 import javax.servlet.*;
 import javax.servlet.http.*;
 
@@ -7,12 +8,23 @@ public class LoginServlet extends HttpServlet
 	  public void doGet(HttpServletRequest request, HttpServletResponse response)
       throws ServletException, IOException
       {
-		  String message = "Hello World!";
-		  // Set response content type
-		  response.setContentType("text/html");
-
-		  // Actual logic goes here.
-		  PrintWriter out = response.getWriter();
-		  out.println("<h1>" + message + "</h1>");
+		  RequestDispatcher view = request.getRequestDispatcher("/html/login.html");
+		  view.forward(request, response);
+		  
       }
+	  
+	  public void doPost(HttpServletRequest request, HttpServletResponse response)
+	  throws ServletException, IOException
+	  {
+		  String username = request.getParameter("USERID");
+		  String password = request.getParameter("PASSWD");
+		  
+	      // Set response content type
+	      response.setContentType("text/html");
+
+	      // Actual logic goes here.
+	      PrintWriter out = response.getWriter();
+	      out.println("<h2>Username:"+username+"</h2>");
+	      out.println("<h2>Password:"+password+"</h2>");
+	  }
 }
